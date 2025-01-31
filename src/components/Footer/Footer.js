befc38e312693ebf3e789fedfc806ed0a064ccba
amoggha03@gmail.com <amoggha03@gmail.com>
2025-01-31 16:24:11 +0530

    website working, added style & basic functionality

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer style={{ backgroundColor: '#333', color: 'white', textAlign: 'center', padding: '20px', marginTop: '20px' }}>
+      <p>&copy; 2023 Anomaly Detection Website. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
